65804a96e21cf59c8c9f5d27a00e5ea40f4c9ce8
Darkwind <somatic20@gmail.com>
2021-06-07 23:24:31 +0100

    added withdrawal function added interface for aave integration added adai abi updated tests

--- contracts/AaveTest.sol
+++ contracts/AaveTest.sol
@@ -1,53 +1,75 @@
-pragma solidity ^0.5.0;
+pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@studydefi/money-legos/aave/contracts/ILendingPool.sol";
-import "@studydefi/money-legos/aave/contracts/ILendingPoolAddressesProvider.sol";
+import "./interfaces/ILendingPool.sol";
+import "./interfaces/ILendingPoolAddressesProvider.sol";
+// import "@studydefi/money-legos/aave/contracts/ILendingPool.sol";
+// import "@studydefi/money-legos/aave/contracts/ILendingPoolAddressesProvider.sol";
+
 // import {IAaveInterface} from "../interfaces/IAaveInterface.sol"
 
-
 contract AaveTest {
-    address constant daiAddress = address(0x6B175474E89094C44Da98b954EedeAC495271d0F);
-    address constant aaveLendingPoolAddressProviderAddress = address(0x24a42fD28C976A61Df5D00D0599C34c4f90748c8);
+    address constant daiAddress =
+        address(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+    address constant aaveLendingPoolAddressProviderAddress =
+        address(0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5);
+    address constant aDaiAddress =
+        address(0x028171bCA77440897B824Ca71D1c56caC55b68A3);
 
-
-    ILendingPoolAddressesProvider provider = ILendingPoolAddressesProvider(aaveLendingPoolAddressProviderAddress); // mainnet address, for other addresses: https://docs.aave.com/developers/developing-on-aave/deployed-contract-instances
-// LendingPool lendingPool = LendingPool(provider.getLendingPool());
-
+    ILendingPoolAddressesProvider provider =
+        ILendingPoolAddressesProvider(aaveLendingPoolAddressProviderAddress); // mainnet address, for other addresses: https://docs.aave.com/developers/developing-on-aave/deployed-contract-instances
     ILendingPool lendingPool = ILendingPool(provider.getLendingPool());
 
     function deposit() public payable {
         IERC20 daiToken = IERC20(daiAddress);
-        uint amountToTransfer = daiToken.allowance(msg.sender, address(this));
-        
-        require(amountToTransfer > 0, 'somto sent');
+        uint256 amountToTransfer =
+            daiToken.allowance(msg.sender, address(this));
+
+        require(amountToTransfer > 0, "dai sent");
         daiToken.transferFrom(msg.sender, address(this), amountToTransfer);
 
         daiToken.approve(provider.getLendingPoolCore(), amountToTransfer);
-        
-        lendingPool.deposit(daiAddress, amountToTransfer, 0);
+
+        lendingPool.deposit(daiAddress, amountToTransfer, msg.sender, 0);
     }
 
-    function borrow(uint amount, uint variableRate) public { 
-        // uint256 variableRate = 2;
-        lendingPool.borrow(daiAddress, amount, variableRate, 0);
+    function withdraw(uint256 amount) public {
+        IERC20 aDaiToken = IERC20(aDaiAddress);
+        uint aDaiBalance = aDaiToken.balanceOf(address(this));
+        require(aDaiBalance >= amount);
+        lendingPool.withdraw(daiAddress, amount, msg.sender);
     }
-    // address constant aaveProtocolAddress =
-    //     address(0x9198F13B08E299d85E096929fA9781A1E3d5d827);
-    // address constant daiAddress =
-    //     address(0x6b175474e89094c44da98b954eedeac495271d0f);
 
-    // IAaveInterface aaveVault = IAaveInterface(aaveProtocolAddress);
-    // IERC20 daiToken = IERC20(daiAddress);
-
-    // function deposit(uint256 amount) public payable {
-    //     daiToken.approve(address(this), amount);
-
-    //     aaveVault.deposit(daiAddress, amount, address(this), 0);
+    // function borrow(uint256 amount) public {
+    //     uint256 rate = 2; //stable is 1, variable is 2
+    //     lendingPool.borrow(daiAddress, amount, rate, 0);
     // }
 
-    // function withdraw(uint256 amount) public {
-    //     // daiToken.approve(address(this), amount);
-    //     aaveVault.withdraw(daiAddress, amount, address(this));
+    // function balance() public returns (uint256, uint256) {
+    //     uint256 currentATokenBalance;
+    //     uint256 currentBorrowBalance;
+    //     uint256 principalBorrowBalance;
+    //     uint256 borrowRateMode;
+    //     uint256 borrowRate;
+    //     uint256 liquidityRate;
+    //     uint256 originationFee;
+    //     uint256 variableBorrowIndex;
+    //     uint256 lastUpdateTimestamp;
+    //     uint256 extra;
+    //     bool usageAsCollateralEnabled;
+    //     (
+    //         currentATokenBalance,
+    //         currentBorrowBalance,
+    //         ,
+    //         ,
+    //         ,
+    //         ,
+    //         ,
+    //         ,
+    //         ,
+    //         ,
+
+    //     ) = lendingPool.getUserReserveData(daiAddress, msg.sender);
+    //     return (currentATokenBalance, currentBorrowBalance);
     // }
 }
--- test/Aave.test.js
+++ test/Aave.test.js
@@ -3,11 +3,16 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3("http://localhost:7545");
 const dai_abi = require('../abi/dai.json');
+const aDai_abi = require('../abi/aDai.json');
 const AaveTest = artifacts.require("AaveTest");
 
 const dai_address = "0x6b175474e89094c44da98b954eedeac495271d0f";
+const a_dai_address = "0x028171bCA77440897B824Ca71D1c56caC55b68A3";
 const unlockedAddress = "0x16463c0fdb6ba9618909f5b120ea1581618c1b9e";
+
 const dai = new web3.eth.Contract(dai_abi, dai_address);
+const aDai = new web3.eth.Contract(aDai_abi, a_dai_address);
+
 
 contract("AaveTest", async accounts => {
     let aave;
@@ -44,18 +49,18 @@ contract("AaveTest", async accounts => {
     });
 
     describe("Aave functions test", () => {
+        
         it('check account has enough  dai', async () => {
             let amount = web3.utils.toWei('100', 'ether');
-            await sendDai(receiverAddress, amount);
+            await sendDai(receiverAddress, amount); //sendDai funds first account with about 100 dai from unlocked address
             let balance = await dai.methods.balanceOf(receiverAddress).call();
-            // balance = web3.utils.toWei('100', 'wei');
             console.log('dai balance: ' + balance);
             const expected = BigInt(100000000000000000000);
             assert.equal(balance, expected, "Must be equal");
         });
 
         it('check amount deposited', async () => {
-            const amount = web3.utils.toWei('10', 'ether');
+            const amount = web3.utils.toWei('50', 'ether');
             let balance = await web3.eth.getBalance(receiverAddress);
 
             console.log(amount, "amount to deposit")
@@ -63,14 +68,63 @@ contract("AaveTest", async accounts => {
 
             console.log(aave.address, 'aave address');
 
-            await dai.methods.approve(aave.address, amount).send({from: receiverAddress});
+            await dai.methods.approve(aave.address, amount).send({from: receiverAddress}); //this gives the contract permission to use funds sent
             let allowanceBalance = await dai.methods.allowance(receiverAddress, aave.address).call(); //owner address first, then receiver address
             console.log(allowanceBalance, 'allowance balance');
-            console.log('approved');
             await aave.deposit();
             console.log('deposited...');
-            let expected = web3.utils.toWei('10', 'ether');
+            let expected = web3.utils.toWei('50', 'ether');
             assert.equal(allowanceBalance, expected);
         });
+
+        it('check a_token balance', async () => {
+            const amount = await aDai.methods.balanceOf(receiverAddress).call();
+            console.log(amount, 'adai balance');
+            const expected = web3.utils.toWei('50', 'ether');
+            const balance = web3.utils.toWei(amount, 'ether');
+            console.log(balance, 'converted adai balance');
+
+            assert.equal(balance, expected);
+        })
+
+        it('withdraw deposited atoken', async () => {
+            const amount = web3.utils.toWei('20', 'ether');
+            await aave.withdraw(amount);
+            
+            const balance = await aDai.methods.balanceOf(receiverAddress).call();
+            console.log(balance, 'converted adai balance after withdrawal');
+            const expected = web3.utils.toWei('30', 'ether');
+            assert.equal(balance, expected);
+        })
+
+        // it('check amount can be borrowed', async () => {            
+        //     const amount = web3.utils.toWei('20', 'ether');
+
+        //     let balanceBefore =await dai.methods.balanceOf(receiverAddress).call();
+        //     console.log("dai balance before borrow ", balanceBefore);
+
+        //     await aave.borrow(amount);
+        //     let balanceAfter = await dai.methods.balanceOf(receiverAddress).call();
+        //     console.log("dai balance after borrow ", balanceAfter);
+
+        //     const expected = BigInt(50000000000000000000);
+        //     assert.equal(expected, balanceAfter);
+        // });
+
+        // it('get supply and borrow balance', async () => {
+        //    const {
+        //     currentATokenBalance : daiLended ,
+        //     currentBorrowBalance : daiBorrowed
+        //    } = await aave.balance();
+
+        //    let lendedAmount = web3.utils.toWei('50', 'ether');
+        //    let borrowedAmount = web3.utils.toWei('20', 'ether');
+
+        //    console.log("dai lended", daiLended);
+        //    console.log("dai borrowed", daiBorrowed);
+
+        //    assert.equal(daiLended, lendedAmount, "dai deposited must be equal");
+        //    assert.equal(daiBorrowed, borrowedAmount, "dai borrowed must be equal");
+        // });
     });
 });
